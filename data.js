4747538ae4949948f6d547a3e4f8239d03f8a90e
garvtayal8hsdpsmzn-art <garv.tayal.8.h.sdpsmzn@gmail.com>
2026-01-20 23:32:25 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,20 @@
+let hotspots = [
+    {
+        id: 1,
+        name: "Minto Bridge",
+        latitude: 28.6357,
+        longitude: 77.2233,
+        ward: "New Delhi",
+        severity: "High"
+    },
+    {
+        id: 2,
+        name: "ITO Crossing",
+        latitude: 28.6289,
+        longitude: 77.2410,
+        ward: "Central Delhi",
+        severity: "Medium"
+    }
+];
+
+module.exports = hotspots;
